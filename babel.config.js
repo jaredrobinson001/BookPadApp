79168b720c8d07d33a66139b34f897d0c3037ced
“jaredrobinson001” <jared.robinson001@gmail.com>
2022-11-12 17:04:01 +0700

    feat: add eslint

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        extensions: [".ts", ".tsx", ".js", ".json"],
+        alias: {},
+      },
+    ],
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-proposal-optional-chaining",
+  ],
 };
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { SafeAreaView, ScrollView, Text } from "react-native";
+
+const App = (): JSX.Element => {
+  return (
+    <SafeAreaView
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <ScrollView>
+        <Text>asdasd</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
