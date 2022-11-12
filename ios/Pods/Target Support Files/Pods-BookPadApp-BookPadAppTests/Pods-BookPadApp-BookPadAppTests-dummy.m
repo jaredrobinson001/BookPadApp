daf7863458d7741679e9a6e3487941234a9ce49e
“jaredrobinson001” <jared.robinson001@gmail.com>
2022-11-12 13:29:34 +0700

    Init project

---
+++ ios/Pods/Target Support Files/Pods-BookPadApp-BookPadAppTests/Pods-BookPadApp-BookPadAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BookPadApp_BookPadAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_BookPadApp_BookPadAppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-BookPadApp/Pods-BookPadApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BookPadApp : NSObject
+@end
+@implementation PodsDummy_Pods_BookPadApp
+@end
