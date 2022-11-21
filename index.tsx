408ee541beec649a941d01cbab5f8fed8a48ebb4
“jaredrobinson001” <jared.robinson001@gmail.com>
2022-11-21 20:58:12 +0700

    feat: modify app context wrapper

--- index.tsx
+++ index.tsx
@@ -2,8 +2,16 @@
  * @format
  */
 
+import { AppContext } from "@core/contexts/MainApp";
+import React from "react";
 import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 import App from "./src/App";
 
-AppRegistry.registerComponent(appName, () => App);
+const app = () => (
+  <AppContext>
+    <App />
+  </AppContext>
+);
+
+AppRegistry.registerComponent(appName, () => app);
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useGlobalLoading, useGlobalSnackBar } from "@core";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AppStack } from "@app/navigator";
 import { Loading } from "@app/components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Snackbar } from "react-native-paper";
-import { AppContext } from "@core/contexts/MainApp";
 
 const queryClient = new QueryClient();
-const Stack = createNativeStackNavigator();
+
 const App = (): JSX.Element => {
   const { IS_LOADING } = useGlobalLoading();
   const {
@@ -19,28 +17,26 @@ const App = (): JSX.Element => {
     hideGlobalSnackBar,
   } = useGlobalSnackBar();
   return (
-    <AppContext>
-      <QueryClientProvider client={queryClient}>
-        <NavigationContainer>
-          <AppStack />
-          <Loading isLoading={IS_LOADING} />
-          <Snackbar
-            visible={IS_SHOW_SNACK_BAR}
-            onDismiss={hideGlobalSnackBar}
-            action={{
-              label: SNACK_BAR_ACTION.label,
-              onPress: () => {
-                // Do something
-                SNACK_BAR_ACTION.onPress();
-              },
-            }}
-            duration={3000}
-          >
-            {SNACK_BAR_MESSAGE}
-          </Snackbar>
-        </NavigationContainer>
-      </QueryClientProvider>
-    </AppContext>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <AppStack />
+        <Loading isLoading={IS_LOADING} />
+        <Snackbar
+          visible={IS_SHOW_SNACK_BAR}
+          onDismiss={hideGlobalSnackBar}
+          action={{
+            label: SNACK_BAR_ACTION.label,
+            onPress: () => {
+              // Do something
+              SNACK_BAR_ACTION.onPress();
+            },
+          }}
+          duration={3000}
+        >
+          {SNACK_BAR_MESSAGE}
+        </Snackbar>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
 
